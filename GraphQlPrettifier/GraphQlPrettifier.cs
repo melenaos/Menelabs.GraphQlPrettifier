60908f7c58e5f017e3d16225ecead184a161719c
melenaos <menelaosvergis@gmail.com>
2024-05-28 19:23:07 +0300

    Initial commit

---
+++ GraphQlPrettifier/GraphQlPrettifier.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+
+namespace Menelabs
+{
+    public class GraphQlPrettifier : IPrettifyGraphQl
+    {
+        private enum StateMachine { Fields, FieldSeparator, Args }
+        private readonly StringBuilder stringBuilder = new();
+        private int indent = 0;
+        private bool mustIdent = false;
+        private StateMachine stateMachine = StateMachine.Fields;
+
+        public string Prettify(string graphQl)
+        {
+            Clear();
+
+
+            for (int i = 0; i < graphQl.Length; i++)
+            {
+                var ch = graphQl[i];
+                if (ch == '{')
+                {
+                    stateMachine = StateMachine.Fields;
+                    // prepend a space on {
+                    if (i - 1 > 0 && graphQl[i - 1] != ' ')
+                        Append(' ');
+
+                    Append(ch);
+
+                    // and append a newline
+                    Indent();
+                    AppendLine();
+                }
+                else if (ch == '}')
+                {
+                    // prepend a newline
+                    Outdent();
+                    AppendLine();
+                    Append(ch);
+                    stateMachine = StateMachine.FieldSeparator;
+
+                }
+                else if (ch == ' ')
+                {
+                    char lastChar = ch;
+                    // skip multiple spaces
+                    for (i++; i < graphQl.Length; i++)
+                    {
+                        lastChar = graphQl[i];
+                        if (lastChar != ' ')
+                            break;
+                    }
+
+                    i--;
+                    stateMachine = StateMachine.FieldSeparator;
+                }
+                else if (ch == '(')
+                {
+                    stateMachine = StateMachine.Args;
+                    Append(ch);
+                }
+                else if (ch == ')')
+                {
+                    Append(ch);
+                    stateMachine = StateMachine.FieldSeparator;
+                }
+                else if (ch == ':' || ch == ',')
+                {
+                    Append(ch);
+                    Append(' ');
+                }
+                // skip quoted string
+                else if (ch == '"')
+                {
+                    Append(ch);
+                    for (i++; i < graphQl.Length; i++)
+                    {
+                        Append(graphQl[i]);
+                        if (graphQl[i] == '"')
+                            break;
+                    }
+                }
+                else if (char.IsLetterOrDigit(ch))
+                {
+                    if (stateMachine == StateMachine.FieldSeparator)
+                    {
+                        AppendLine();
+                        stateMachine = StateMachine.Fields;
+                    }
+
+                    Append(ch);
+                }
+
+                else
+                {
+                    Append(ch);
+                }
+            }
+
+            return GetPrettyQuery();
+        }
+
+        private string GetPrettyQuery()
+        {
+            return stringBuilder.ToString();
+        }
+
+        private void Clear()
+        {
+            stringBuilder.Clear();
+            indent = 0;
+            mustIdent = false;
+        }
+        private void Indent()
+        {
+            indent++;
+        }
+
+        private void Outdent()
+        {
+            indent--;
+        }
+
+        public void AppendLine()
+        {
+            stringBuilder.AppendLine();
+            mustIdent = true;
+        }
+
+        public void Append(char query)
+        {
+            AddIndentation();
+            stringBuilder.Append(query);
+        }
+
+        public void Append(string query)
+        {
+            AddIndentation();
+            stringBuilder.Append(query);
+        }
+
+        private void AddIndentation()
+        {
+            if (mustIdent)
+            {
+                for (int i = 0; i < indent; i++)
+                {
+                    stringBuilder.Append("  ");
+                }
+                mustIdent = false;
+            }
+        }
+    }
+}
---
+++ TestProject/GraphQlPrettifier.Tests.cs
@@ -0,0 +1,101 @@
+
+using Menelabs;
+
+namespace TestProject
+{
+    public class GraphQlPrettifierUnitTest
+    {
+        [Fact]
+        public void SimpleQuery()
+        {
+            var prettifier = ProvidePrettifier();
+            var pretty = prettifier.Prettify("query{order(id:\"gid:unique_id\"){name}}");
+
+            Assert.Equal(@"query {
+  order(id: ""gid:unique_id"") {
+    name
+  }
+}",
+pretty);
+        }
+
+        [Fact]
+        public void MultipleFields()
+        {
+            var prettifier = ProvidePrettifier();
+            var pretty = prettifier.Prettify("query{order(id:\"gid:unique_id\"){name id code}}");
+
+            Assert.Equal(@"query {
+  order(id: ""gid:unique_id"") {
+    name
+    id
+    code
+  }
+}",
+pretty);
+        }
+
+        [Fact]
+        public void NestedObjects()
+        {
+            var prettifier = ProvidePrettifier();
+            var pretty = prettifier.Prettify("query{order(id:\"gid:unique_id\"){name id image{width height url}}}");
+
+            Assert.Equal(@"query {
+  order(id: ""gid:unique_id"") {
+    name
+    id
+    image {
+      width
+      height
+      url
+    }
+  }
+}",
+pretty);
+        }
+
+        [Fact]
+        public void NestedObjectsWithArguments()
+        {
+            var prettifier = ProvidePrettifier();
+            var pretty = prettifier.Prettify("query{order(id:\"gid:unique_id\"){name id image(size:BIG,maxWidth:1000){width height url}product title}}");
+
+            Assert.Equal(@"query {
+  order(id: ""gid:unique_id"") {
+    name
+    id
+    image(size: BIG, maxWidth: 1000) {
+      width
+      height
+      url
+    }
+    product
+    title
+  }
+}",
+pretty);
+        }
+
+        [Fact]
+        public void MultipleArgs()
+        {
+            var prettifier = ProvidePrettifier();
+            var pretty = prettifier.Prettify("query{order(id:\"gid:unique_id\",max:5,min:2){name id code}}");
+
+            Assert.Equal(@"query {
+  order(id: ""gid:unique_id"", max: 5, min: 2) {
+    name
+    id
+    code
+  }
+}",
+pretty);
+        }
+
+        private IPrettifyGraphQl ProvidePrettifier()
+        {
+            return new GraphQlPrettifier();
+        }
+    }
+}
